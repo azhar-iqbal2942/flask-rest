49578cba09bc26a01defcb7d083de3b3ade4fbe3
Azhar Iqbal <azhariqbal2942@gmail.com>
2022-10-14 02:06:14 +0500

    Dockarized app. Added Schema validation with marshmallow and Code is now in OOP.

--- app.py
+++ app.py
@@ -1,37 +1,21 @@
-from flask import Flask, request
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = 1
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": stores}
+api = Api(app)
 
-
-@app.post("/store")
-def create_store():
-    request_data = request.get_json()
-    stores.append({"name": request_data.get("name"), "items": []})
-    return {"message": request_data}, 201
-
-
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {
-                "name": request_data.get("name"),
-                "price": request_data.get("price"),
-            }
-            store["items"].append(new_item)
-            return {"message": "Item Created successfully", "data": new_item}, 201
-    return {"message": "Store Not Found"}, 404
-
-
-@app.get("/store/<string:name>/item")
-def get_store_items(name):
-    items = [store for store in stores if store["name"] == name]
-    return {"items": items}, 200
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
---
+++ resources/store.py
@@ -0,0 +1,48 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+
+from db import stores
+from schema import StoreSchema
+
+
+blp = Blueprint("stores", __name__, description="Operations on store")
+
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        try:
+            return stores[store_id]
+        except KeyError:
+            abort(404, message="Store not found.")
+
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {"message": "Store deleted."}
+        except KeyError:
+            abort(404, message="Store not found.")
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(self):
+        return stores.values()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
+    def post(self, store_data):
+
+        for store in stores.values():
+            if store_data["name"] == store["name"]:
+                abort(400, message=f"{store_data.get('name')} already exist")
+
+        store_id = uuid.uuid4().hex
+        store = {**store_data, "id": store_id}
+        stores[store_id] = store
+
+        return store
